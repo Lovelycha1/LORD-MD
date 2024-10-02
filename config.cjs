7afa5af5852caddc6925280fc030690e955bb3e0
Lovelycha1 <agboolamorenikeji472@gmail.com>
2024-10-02 16:03:15 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&m7LR9uYY",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&3HMhnp7f",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
